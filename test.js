2d0a83a3baf5508c2218c285d3af745b8c709197
Jorgi Saputra <saputrajhorgy@gmail.com>
2024-04-21 14:14:47 +0700

    Belajar git Commit

---
+++ test.js
@@ -0,0 +1,44 @@
+Crystal Maiden
+Puck
+Strom Spirit
+Windranger
+Zeus
+Lina
+Shadow Shaman
+Tinker
+Nature’s Prophet
+Enchantress
+Jakiro
+Chen
+Silencer
+Ogre Magi
+Rubick
+Disruptor
+Keeper of The Light
+Skywrath Mage
+Oracle
+Techies
+Bane
+Lich
+Lion
+Witch Doctor
+Enigma
+Necrophos
+Warlock
+Queen of Pain
+Death Prophet
+Pugna
+Dazzle
+Lesrach
+Dark Seer
+Batrider
+Ancient Apparition
+Invoker
+Outworld Devourer
+Shadow Demon
+Visage
+Winter Wyvern
+Rubick
+Dark Willow
+Grimstroke
+Void Spirit
